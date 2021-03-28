e91be40eef2fe36312b91bf0c70f871e7e82ea15
Richard Cho <richardcho100@gmail.com>
2021-03-28 17:50:13 -0400

    Add btn & functionality to delete blogs

--- src/Home.js
+++ src/Home.js
@@ -6,11 +6,17 @@ const Home = () => {
     { id: 1, title: 'All About Lattes', author: 'Barista Bob', body: "It's never too late for a latte..." },
     { id: 2, title: 'Frappuccino Fridays', author: 'Connoisseur Cathy', body: "Happy Friday! I can't believe it's finally friday..." },
     { id: 3, title: 'No Cap for Cappucinos' , author: 'Barista Bob', body: 'There are a lot of differing opinions on cappucinos...' }
-  ])
+  ]);
+
+  const handleDelete = (blogID) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== blogID);
+    setBlogs(newBlogs);
+  };
+
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
     </div>
   );
 }
