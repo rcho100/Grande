ff31b79ea967ae6db0aaa5b587d36c36b4cffe8d
Richard Cho <richardcho100@gmail.com>
2021-03-28 17:12:01 -0400

    Add blogs to state & render to the DOM

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,21 @@
+import { useState } from 'react';
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { id: 1, title: 'All About Lattes', author: 'Barista Bob', body: "It's never too late for a latte..." },
+    { id: 2, title: 'Frappuccino Fridays', author: 'Connoisseur Cathy', body: "Happy Friday! I can't believe it's finally friday..." },
+    { id: 3, title: 'No Cap for Cappucinos' , author: 'Barista Bob', body: 'There are a lot of differing opinions on cappucinos...' }
+  ])
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      {blogs.map(blog => (
+        <div className="blog-preview" key={blog.id} >
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+        </div>
+      ))}
     </div>
   );
-};
-
+}
 export default Home;
