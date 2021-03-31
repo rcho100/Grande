8a965791c61513b2ac6466dc15160e7fd05bc205
Richard Cho <richardcho100@gmail.com>
2021-03-30 23:32:34 -0400

    Create custom hook to hold state & fetch logic

--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,9 @@
-import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then(res => {
-        if (!res.ok) { // error coming back from server
-          throw Error('Could not fetch the data for that resource.')
-        }
-        return res.json();
-      })
-      .then(data => {
-        setBlogs(data);
-        setIsLoading(false);
-        setError(null);
-      })
-      .catch((err) => {
-        // auto catches network / connection error
-        setIsLoading(false);
-        setError(err.message);
-      });
-  }, []);
+  const { data: blogs, isLoading, error } = useFetch("http://localhost:8000/blogs");
+  // grabbing data but naming it blogs in this context
 
   return (
     <div className="home">
---
+++ src/useFetch.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => {
+        if (!res.ok) { // error coming back from server
+          throw Error('Could not fetch the data for that resource.')
+        }
+        return res.json();
+      })
+      .then(data => {
+        setData(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        // auto catches network / connection error
+        setIsLoading(false);
+        setError(err.message);
+      });
+  }, [url]);
+
+  return { data, isLoading, error };
+}
+ 
+export default useFetch;
