872892e6294bad080d40c539c9d9e45e62684363
Richard Cho <richardcho100@gmail.com>
2021-04-09 23:59:41 -0400

    Add in route parameters

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,15 +1,18 @@
-const BlogList = ({ blogs, title }) => {
+import { Link } from 'react-router-dom';
+
+const BlogList = ({ blogs }) => {
   return (
-    <div>
-      <h2>{ title }</h2>
+    <div className="blog-list">
       {blogs.map(blog => (
-        <div className="blog-preview" key={ blog.id } >
-          <h2>{ blog.title }</h2>
-          <p>Written by { blog.author }</p>
+        <div className="blog-preview" key={blog.id} >
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{ blog.title }</h2>
+            <p>Written by { blog.author }</p>
+          </Link>
         </div>
       ))}
     </div>
-  )
+  );
 }
-
-export default BlogList
+ 
+export default BlogList;
